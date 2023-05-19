8e3e8601592380fe8f777c2b7e3f74cf531ed65d
Vidita Kumari <kvidita@users.noreply.github.com>
2023-05-19 23:56:46 +0530

    resolved repeated bird's icon display

--- src/lost-bird.js
+++ src/lost-bird.js
@@ -5,11 +5,16 @@ class creature {
   #x;
   #y;
   #icon;
+  #prevX;
+  #prevY;
 
   constructor(icon, xCordinate, yCordinate) {
     this.#x = xCordinate;
     this.#y = yCordinate;
     this.#icon = icon;
+    this.#prevX = this.#x;
+    this.#prevY = this.#y;
+
   }
 
   xUp() {
@@ -36,7 +41,17 @@ class creature {
     return this.#icon;
   }
 
+  get previousX() {
+    return this.#prevX;
+  }
+
+  get previousY() {
+    return this.#prevY;
+  }
+
   move(direction) {
+    this.#prevX = this.#x;
+    this.#prevY = this.#y;
     this.decrementY();
     if (direction === "w") {
       this.xUp();
@@ -48,17 +63,26 @@ class creature {
 }
 
 // const initializeGame = () => {};
+const readNextMove = () => {
+  const fileContent = fs.readFileSync("./resource/players-input.txt", "utf-8");
+  const [currentMove] = fileContent.split("\n").slice(-1);
+  return currentMove;
+};
 
-const lostBird = function () {
+const display = (list) => {
+  console.log(list.join("\n"));
+};
+
+const lostBird = function() {
   const grid = new Array(100).fill(" ");
   const space = chunk(grid, 20);
   const bird = new creature("🕊", 2, 19);
-
   const nest = "🪺";
   space[2][0] = nest;
 
   setInterval(() => {
     console.clear();
+    space[bird.previousX][bird.previousY] = " ";
     space[bird.x][bird.y] = bird.icon;
     display(space);
     console.log(bird.y);
@@ -67,16 +91,7 @@ const lostBird = function () {
   }, 3000);
 };
 
-const display = (list) => {
-  console.log(list.join("\n"));
-};
 
-const readNextMove = () => {
-  const fileContent = fs.readFileSync("./resource/players-input.txt", "utf-8");
-  const [currentMove] = fileContent.split("\n").slice(-1);
-  return currentMove;
-};
-
-const moveBird = () => {};
+const moveBird = () => { };
 
 lostBird();
--- test/lost-bird-test.js
+++ test/lost-bird-test.js
@@ -1,13 +1,13 @@
-// const stdinput = setInterval(() => {
-//   process.stdin.setEncoding("utf-8");
-//   const input = process.stdin.read();
-//   if (input) {
-//     if (input.trim() === ".exit") {
-//       clearInterval(stdinput);
-//     }
-//     console.log(input);
-//   }
-// }, 500);
+const stdinput = setInterval(() => {
+  process.stdin.setEncoding("utf-8");
+  const input = process.stdin.read();
+  if (input) {
+    if (input.trim() === ".exit") {
+      clearInterval(stdinput);
+    }
+    console.log(input);
+  }
+}, 500);
 
 const writeStdout = () => {
   const num = 1;
