ed450c14bf5d4df77bace726983049583c2803a4
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-22 23:39:31 +0530

    Clearing bird icon bug on movement, refactored navigate function

--- src/lost-bird.js
+++ src/lost-bird.js
@@ -3,20 +3,21 @@ const { Bird } = require("./bird.js");
 const { display, readNextMove } = require("./interface.js");
 
 const navigate = (space, bird) => {
+  
+  bird.flyForward();
+  const currentMove = readNextMove();
+  space[bird.previousX][bird.previousY] = " ";
+  
+  if (currentMove) {
+    bird.move(currentMove);
+  }
+
   console.clear();
   space[bird.previousX][bird.previousY] = " ";
   space[bird.x][bird.y] = bird.icon;
 
   display(space);
-
-  console.log(bird.y);
-  bird.flyForward();
-  const currentMove = readNextMove();
-
-  if (currentMove) {
-    bird.move(currentMove);
-  }
-}
+};
 
 const runLostBird = function() {
   const grid = new Array(100).fill(" ");
